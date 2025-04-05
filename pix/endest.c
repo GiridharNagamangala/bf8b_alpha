7d18cf1fe5521d3573b55bd1e21cac543bf8a0a8
GiridharNagamangala <nagamangala.giridhar@gmail.com>
2025-04-05 14:34:18 +0530

    Created endest.c and added to header file

---
+++ pix/endest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int endex (char* string) {
+    int endest = 0;
+    do { endest++;} while (string[endest] != '\0');
+    return endest;
+}
--- pix/supf.h
+++ pix/supf.h
@@ -6,3 +6,4 @@ int isValidNumber(const char *string);
 int mapWordToNumber(const char word[50]);
 void convert_base(const char *input, int base_in, int base_out, char *output);
 char* binConverter(char *string);
+int endex(char* string);
